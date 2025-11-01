74da61811456dd810723f45912748bc82dc5dfb6
JanGomez09 <jan.estebanrg@gmail.com>
2025-11-01 09:56:40 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+function clk(elem) {
+
+
+  switch (elem) {
+    case "FNAF":
+      
+      
+      
+      window.location.href = "index2.html";
+
+      break;
+
+    case "TRON":
+      window.location.href = "index3.html";
+      break;
+
+    case "SMB2":
+      window.location.href = "index4.html";
+
+      break;
+
+    case "SHK5":
+      window.location.href = "index5.html";
+
+      break;
+
+    case "TOY5":
+      window.location.href = "index6.html";
+
+      break;
+
+    case "DOOM":
+      window.location.href = "index7.html";
+      break;
+
+    case "FIII":
+      window.location.href = "index8.html";
+
+      break;
+
+    case "SCR7":
+      window.location.href = "index9.html";
+      break;
+
+      
+  }
+}
+
+
